8b0001fbf8d458cfa9483b841acd8618f3ed2df4
mook1122 <kmuk223@gmail.com>
2024-07-12 02:05:06 +0900

    세팅

--- src/App.js
+++ src/App.js
@@ -3,9 +3,53 @@ import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { Footer, Header } from './Route/Layout'
 
-const Btn = styled.button`
-color:white;
-background-color:green;
+
+const BannerTitle = styled.div`
+
+text-align: center;
+
+font-size: 5vw;
+margin-top: 4%;
+
+font-family: "Nanum Brush Script", cursive;
+  font-weight: 400;
+  font-style: normal;
+`;
+
+
+const Banner = styled.div`
+
+margin-top: 4%;
+margin-bottom: 4%;
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+`;
+
+const BannerItem = styled.div`
+
+  max-width: 1300px;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100%;
+    max-width: 450px;
+
+  }
+
+  div {
+    /* padding: 0 40px; */
+    width: 50%;
+
+    font-size: 30px;
+  }
+
 `;
 
 function App() {
@@ -15,16 +59,48 @@ function App() {
   return (
     <div>
 
-      <Routes>
-        <Route path='/' element={
-          <>    
-          <Btn onClick={() => { navigate('/11') }}>Simple</Btn>
-          <button>asd</button>
-          </>
-        }/>
+      <Header></Header>
 
-        <Route path='/11' element={<Header></Header>} />
+      <Routes>
+
+        <Route path='/' element={
+          <>
+
+            <BannerTitle>
+              일생에 가장 아름다운 순간
+            </BannerTitle>
+
+            <Banner>
+
+              <BannerItem>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                  <img src={process.env.PUBLIC_URL + `/img/banner.jpg`}></img>   
+                </div>
+
+                <div>
+                  <p>
+                    무료로 만들어보세요!
+                  </p>
+                  <p>
+                    샘플을 찾아보고
+                  </p>
+                  <p>
+                    마음에 드는 청첩장을 골라보세요.
+                  </p>
+                </div>
+
+
+              </BannerItem>
+
+            </Banner>
+
+
+          </>
+        } />
+
       </Routes>
+
+      <Footer></Footer>
 
 
 
--- src/Route/Layout.js
+++ src/Route/Layout.js
@@ -1,12 +1,27 @@
 import styled from 'styled-components'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+
 
 const HeaderNav = styled.header`
   width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
   background-color: white;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    max-width: 1200px;
+    width: 100%;
+  }
 `;
 
 const Title = styled.h1`
@@ -16,6 +31,9 @@ const Title = styled.h1`
   font-family: "Gugi", sans-serif;
   font-weight: 400;
   font-style: normal;
+
+  cursor: pointer;
+
   img {
     width:40px;
     margin-right:10px;
@@ -31,34 +49,52 @@ const Menu = styled.nav`
       margin-left: 20px;
       font-size : 18px;
       font-weight: 700;
+      cursor: pointer;
     }
   }
 `;
 
 function Header() {
+
+  let navigate = useNavigate()
+
   return (
     <HeaderNav>
-      <Title>
-        <img src={process.env.PUBLIC_URL + `/img/logo.png`}></img>
-        드림데이
+      <div>
+
+        <Title onClick={() => { navigate('/') }}>
+          <img src={process.env.PUBLIC_URL + `/img/logo.png`}></img>
+          드림데이
         </Title>
-      <Menu>
-        <ul>
-          <li>모바일 청첩장</li>
-          <li>감사장</li>
-          <li>샘플</li>
-        </ul>
-      </Menu>
+        <Menu>
+          <ul>
+            <li onClick={() => { navigate('/invitation') }}>모바일 청첩장</li>
+            <li onClick={() => { navigate('/thanks-card') }}>감사장</li>
+            <li onClick={() => { navigate('/sample') }}>샘플</li>
+          </ul>
+        </Menu>
+      </div>
     </HeaderNav>
   );
 }
 
 
+let Foot = styled.footer`
+
+background-color: #2C2C2C;
+width: 100%;
+height: 50px;
+
+position: relative;
+transform: (translateY(0%));
+
+`;
+
 function Footer() {
   return (
-    <div className="">
-      hi
-    </div>
+    <Foot>
+
+    </Foot>
   );
 }
 
