118ecb741789f658cc814c049cd4c788b6200571
mook1122 <kmuk223@gmail.com>
2024-07-15 22:25:24 +0900

    테마 컴포넌트 작업

--- src/Route/Invitation.js
+++ src/Route/Invitation.js
@@ -37,6 +37,26 @@ height: 100%;
 width: 400px;
 border-radius: 10px;
 background-color: ${props => props.bg};
+padding: 30px;
+text-align: center;
+
+overflow: scroll;
+
+  /* Hide scrollbar for WebKit-based browsers (e.g., Chrome, Safari) */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge, and Firefox */
+  -ms-overflow-style: none;  /* Internet Explorer and Edge */
+  scrollbar-width: none;  /* Firefox */
+/* 
+* {
+    font-family: "Nanum Myeongjo", serif;
+  font-weight: 400;
+  font-style: normal;
+} */
+
 
 `;
 
@@ -44,9 +64,7 @@ const Selector = styled.div`
 
     margin: 20px;
     height: 100%;
-    border: 2px solid black;
     width: 600px;
-    padding: 10px; // 지워야함
 
     display: flex;
     flex-direction: column;
@@ -58,9 +76,25 @@ const Selector = styled.div`
 
 `;
 
+const SampleHeader = styled.div`
+`;
+
+const SampleTitle = styled.div`
+
+color: ${props => props.titleColor};
+
+
+* {
+  font-family: "Nanum Myeongjo", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+`;
 
 function Invitation() {
-    const [bg, setBg] = useState('white');
+    const [bg, setBg] = useState('#FAFAFA');
+    const [titleColor, setTitleColor] = useState('black');
 
     const [openSections, setOpenSections] = useState({
         theme: false,
@@ -78,15 +112,57 @@ function Invitation() {
 
     return (
         <>
+
+            <GlobalStyle></GlobalStyle>
+
             <Container>
                 <Sample bg={bg}>
                     {/* 샘플 컴포넌트 내용 */}
+
+                    <SampleHeader>
+
+                        <SampleTitle titleColor={titleColor}>
+                            <p style={{ fontWeight: 'bold' }}>THE MARRIAGE</p>
+                        </SampleTitle>
+                        <br></br>
+
+                        <h1>날짜 표기 해야함</h1>
+
+                        <br></br>
+                        <br></br>
+
+                        <p>이름작성 , 이름작성 결혼합니다.</p>
+
+                        {/* 이미지칸 생성 해야함 */}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <p>2024. 07. 15. Monday 12:00 PM</p>
+                        {/* 날짜 API이용해서 바꿔야함 */}
+                    </SampleHeader>
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <SampleTitle titleColor={titleColor}>
+                        <p style={{ fontSize: '11px' }}>I N V I T A T I ON</p>
+                        <br></br>
+                        <p style={{ fontWeight: 'bold' }}>소중한 분들을 초대 합니다.</p>
+                    </SampleTitle>
+
+
                 </Sample>
 
                 <Selector>
-                    <ThemeSection bg={bg} setBg={setBg} openSection={openSections.theme} toggleSection={() => toggleSection('theme')} />
-                        <br></br>
-                    <BasicInfoSection openSection={openSections.basicInfo} toggleSection={() => toggleSection('basicInfo')} />
+                    <ThemeSection bg={bg} setBg={setBg}
+                        titleColor={titleColor} setTitleColor={setTitleColor}
+                        openSection={openSections.theme} toggleSection={() => toggleSection('theme')} />
+                    <br></br>
+                    <BasicInfoSection
+                        titleColor={titleColor} setTitleColor={setTitleColor}
+                        openSection={openSections.basicInfo} toggleSection={() => toggleSection('basicInfo')} />
                     {/* <MainScreenSection openSection={openSections.mainScreen} toggleSection={() => toggleSection('mainScreen')} />
                     <CalendarSection openSection={openSections.calendar} toggleSection={() => toggleSection('calendar')} /> */}
                 </Selector>
--- src/section/Theme.js
+++ src/section/Theme.js
@@ -21,10 +21,40 @@ const DetailItem = styled.div`
     width: 100%;
     max-height: ${props => (props.show ? '500px' : '0')};
     overflow: hidden;
-    transition: max-height 0.3s ease-in-out;
+    transition: max-height  ease-in-out;
+
+    > div {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+    }
+
 `;
 
-function ThemeSection({ bg, setBg, openSection, toggleSection }) {
+
+const ColorOption = styled.label`
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 5px;
+    border: 2px solid #fff;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+
+    input {
+        display: none;
+    }
+
+
+    input:checked + & {
+        border: 2px solid #aaa;
+    }
+`;
+
+function ThemeSection({ bg, setBg, titleColor, setTitleColor, openSection, toggleSection }) {
+    
     return (
         <div>
             <ToggleBar onClick={toggleSection}>
@@ -32,9 +62,100 @@ function ThemeSection({ bg, setBg, openSection, toggleSection }) {
             </ToggleBar>
 
             <DetailItem show={openSection}>
-                <button onClick={() => setBg('lightblue')}>Light Blue</button>
-                <button onClick={() => setBg('lightgreen')}>Light Green</button>
-                <button onClick={() => setBg('lightcoral')}>Light Coral</button>
+
+                <div>
+                    <p>배경 색상</p>
+                    <div style={{ marginLeft: '15px' }}>
+                        <input
+                            type="radio"
+                            id="#FAFAFA"
+                            name="theme"
+                            value="#FAFAFA"
+                            onChange={() => setBg('#FAFAFA')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#FAFAFA" htmlFor="#FAFAFA" />
+
+                        <input
+                            type="radio"
+                            id="#FEF7F7"
+                            name="theme"
+                            value="#FEF7F7"
+                            onChange={() => setBg('#FEF7F7')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#FEF7F7" htmlFor="#FEF7F7" />
+
+                        <input
+                            type="radio"
+                            id="#F2ECE6"
+                            name="theme"
+                            value="#F2ECE6"
+                            onChange={() => setBg('#F2ECE6')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#F2ECE6" htmlFor="#F2ECE6" />
+
+                        <input
+                            type="radio"
+                            id="#F1EDEE"
+                            name="theme"
+                            value="#F1EDEE"
+                            onChange={() => setBg('#F1EDEE')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#F1EDEE" htmlFor="#F1EDEE" />
+
+                    </div>
+                </div>
+
+                <div>
+                    <p>강조 색상</p>
+                    <div style={{ marginLeft: '15px' }}>
+                        <input
+                            type="radio"
+                            id="black"
+                            name="theme"
+                            value="black"
+                            onChange={() => setTitleColor('black')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="black" htmlFor="black" />
+
+                        <input
+                            type="radio"
+                            id="#F8C3C3"
+                            name="theme"
+                            value="#F8C3C3"
+                            onChange={() => setTitleColor('#F8C3C3')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#F8C3C3" htmlFor="#F8C3C3" />
+
+                        <input
+                            type="radio"
+                            id="#BE9164"
+                            name="theme"
+                            value="#BE9164"
+                            onChange={() => setTitleColor('#BE9164')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#BE9164" htmlFor="#BE9164" />
+
+                        <input
+                            type="radio"
+                            id="#CFA7B4"
+                            name="theme"
+                            value="#CFA7B4"
+                            onChange={() => setTitleColor('#CFA7B4')}
+                            style={{ display: 'none' }}
+                        />
+                        <ColorOption color="#CFA7B4" htmlFor="#CFA7B4" />
+
+                    </div>
+                </div>
+
+
             </DetailItem>
         </div>
     );
