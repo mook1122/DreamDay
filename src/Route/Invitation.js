148400575c9a54fa0ebdc7805e5f831e4ccbd729
mook1122 <kmuk223@gmail.com>
2024-07-21 21:39:00 +0900

    인사말 작업 완료

--- src/Route/Invitation.js
+++ src/Route/Invitation.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components'
 
 import ThemeSection from '../section/Theme';
@@ -99,6 +99,18 @@ const MainImgBox = styled.div`
     height: 100%;
     max-height: 450px;
   }
+
+  div {
+    width: 100%;
+    height: 300px;
+    border: 1px solid black;
+    background-color: #eee;
+    opacity: 0.5;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 
@@ -108,21 +120,27 @@ const SampleTitle = styled.div.withConfig({
 
 color: ${props => props.titlecolor};
 
-
 * {
   font-family: "Nanum Myeongjo", serif;
   font-weight: 400;
   font-style: normal;
 }
-
 `;
+
+const SampleContent = styled.p`
+    white-space: pre-line;
+    line-height: 2;
+    font-size: 14px;
+
+    color: #585858;
+`;
+
 
 function Invitation() {
 
     // 임시 저장하기
     const [totaldata, setTotaldata] = useState()
 
-    // console.log(totaldata);
     const Upload = () => {
         setTotaldata({
             bg: bg,
@@ -138,11 +156,11 @@ function Invitation() {
     // 테마 백그라운드 컬러
     const [bg, setBg] = useState('#FAFAFA');
     // 테마 강조 컬러
-    const [titlecolor, setTitlecolor] = useState('black');
+    const [titlecolor, setTitlecolor] = useState('#F8C3C3');
 
     // 신랑,신부 기본 정보
     const [man, setMan] = useState({
-        me: '',
+        me: '김OO',
         father: '',
         mom: '',
         fatherDeceased: '',
@@ -165,7 +183,7 @@ function Invitation() {
     };
 
     const [woman, setWoman] = useState({
-        me: '',
+        me: '이OO',
         father: '',
         mom: '',
         fatherDeceased: '',
@@ -187,9 +205,28 @@ function Invitation() {
     };
 
 
+    // 인사말 
+    const [introtitle, setIntrotitle] = useState('');
+    const [introcontent, setIntrocontent] = useState('');
+
+    useEffect(() => {
+        setIntrotitle('소중한 분들을 초대합니다.');
+        setIntrocontent(`저희 두 사람의 작은 만남이
+사랑의 결실을 이루어
+소중한 결혼식을 올리게 되었습니다.
+            
+평생 서로 귀하게 여기며
+첫 마음 그대로 존중하고 배려하며 살겠습니다.
+            
+오로지 믿음과 사랑을 약속하는 날
+오셔서 축복해 주시면 더 없는 기쁨으로
+간직하겠습니다.`);
+    }, []);
+
+    console.log(introcontent);
 
     // 대표 이미지 url
-    const [previewUrl, setPreviewUrl] = useState('123');
+    const [previewUrl, setPreviewUrl] = useState('');
 
     // 아코디언 섹션
     const [openSections, setOpenSections] = useState({
@@ -229,7 +266,15 @@ function Invitation() {
                         <br></br>
 
                         <MainImgBox >
-                            <img src={previewUrl} ></img>
+                            {
+                                previewUrl === ''
+                                    ?
+                                    <div>
+                                        <p>대표 이미지</p>
+                                    </div>
+                                    :
+                                    <img src={previewUrl} ></img>
+                            }
                         </MainImgBox>
 
                         <br></br>
@@ -252,8 +297,15 @@ function Invitation() {
                     <SampleTitle titlecolor={titlecolor}>
                         <p style={{ fontSize: '11px' }}>I N V I T A T I ON</p>
                         <br></br>
-                        <p style={{ fontWeight: 'bold' }}>소중한 분들을 초대 합니다.</p>
+                        <p style={{ fontWeight: 'bold' }}>{introtitle}</p>
+                        <br></br>
+                        <br></br>
+
                     </SampleTitle>
+
+                    <SampleContent>
+                        {introcontent}
+                    </SampleContent>
 
 
                 </Sample>
@@ -274,12 +326,15 @@ function Invitation() {
                         previewUrl={previewUrl} setPreviewUrl={setPreviewUrl}
                         openSection={openSections.mainScreen} toggleSection={() => toggleSection('mainScreen')} />
                     <br></br>
-                    
-                    <IntroMent openSection={openSections.intro} toggleSection={() => toggleSection('intro')} />
+
+                    <IntroMent
+                        introtitle={introtitle} setIntrotitle={setIntrotitle} introcontent={introcontent} setIntrocontent={setIntrocontent}
+                        openSection={openSections.intro} toggleSection={() => toggleSection('intro')} />
+
                 </Selector>
 
 
-                <button onClick={() => { Upload() }}>테스트 저장</button>
+                {/* <button onClick={() => { Upload() }}>테스트 저장</button> */}
             </Container>
         </>
     );
